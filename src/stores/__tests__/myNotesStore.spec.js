1620f165580450e96db4b0bcad26181379b58803
Oleg1989 <ivanishin9891@gmail.com>
2024-07-11 21:26:08 +0300

    Added unit tests to myNotesStore.js

---
+++ src/stores/__tests__/myNotesStore.spec.js
@@ -0,0 +1,47 @@
+import { createPinia, setActivePinia } from 'pinia'
+import { beforeEach, describe, expect, test } from 'vitest'
+import { useMyNotesStore } from '@/stores/myNotesStore'
+
+describe('My notes store', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  test('that the myNotes starts at []', () => {
+    const myNotesStore = useMyNotesStore()
+
+    expect(myNotesStore.myNotes).toEqual([])
+  })
+
+  test('that the myNotes add note', () => {
+    const myNotesStore = useMyNotesStore()
+
+    myNotesStore.addNote({ title: 'First note', text: 'Note text' })
+
+    expect(myNotesStore.myNotes).not.toEqual([])
+    expect(myNotesStore.myNotes.length).toBe(1)
+  })
+
+  test('that the myNotes delete note', () => {
+    const myNotesStore = useMyNotesStore()
+
+    myNotesStore.deleteNote(myNotesStore.myNotes[0].id)
+
+    expect(myNotesStore.myNotes).toEqual([])
+  })
+
+  test('that the myNotes update note', () => {
+    const myNotesStore = useMyNotesStore()
+
+    myNotesStore.addNote({ title: 'First note', text: 'Note text' })
+    const note = {
+      id: myNotesStore.myNotes[0].id,
+      title: 'Update title',
+      text: 'Update text'
+    }
+    myNotesStore.updateNote(note)
+
+    expect(myNotesStore.myNotes[0].title).toBe(note.title)
+    expect(myNotesStore.myNotes[0].text).toBe(note.text)
+  })
+})
--- src/stores/__tests__/myToDoListsStore.spec.js
+++ src/stores/__tests__/myToDoListsStore.spec.js
@@ -51,3 +51,15 @@ describe('My to-do lists store', () => {
     expect(myToDoListsStore.myToDoLists[0].title).toBe(toDo.title)
   })
 })
+
+// import { describe, it, expect } from 'vitest'
+
+// import { mount } from '@vue/test-utils'
+// import HelloWorld from '../HelloWorld.vue'
+
+// describe('HelloWorld', () => {
+//   it('renders properly', () => {
+//     const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+//     expect(wrapper.text()).toContain('Hello Vitest')
+//   })
+// })
